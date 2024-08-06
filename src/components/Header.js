5b18bb4ff5220b8ad1c3a676fac6747df02460a3
soufiane_mob <sammahsoufaine01@gmail.com>
2024-08-06 19:22:57 +0100

    changing the app title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="app-header">
       <img src="/public/logo512.png" alt="React logo" />
-      <h1>The React Quiz</h1>
+      <h1>React Quiz</h1>
     </header>
   );
 }
