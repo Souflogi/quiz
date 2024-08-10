f465c708e014b0adf4fde93921cf6800e4895c37
soufiane <sammahsoufiane01@gmail.com>
2024-08-10 10:26:10 +0100

    fixied the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="/public/logo512.png" alt="React logo" />
+      <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="React logo" />
       <h1>React Quiz</h1>
     </header>
   );
